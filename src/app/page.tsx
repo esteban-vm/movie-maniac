a510544f2a2c77be099f1aa646489d08c94d4501
esteban-vm <estebanvm90@outlook.com>
2024-02-12 09:16:41 -0300

    set initial style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <h1 className='text-3xl font-bold text-green-500'>Movie Maniac</h1>
+    <main className='grid grid-rows-2'>
+      <span>Navbar Content</span>
+      <span>Main Content</span>
     </main>
   )
 }
