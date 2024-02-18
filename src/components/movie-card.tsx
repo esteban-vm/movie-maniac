b700ffebe6d0d98dd97ed12e624e3540e6508c1b
esteban-vm <estebanvm90@outlook.com>
2024-02-18 13:13:44 -0300

    create api route for popular movie list

--- src/components/movie-card.tsx
+++ src/components/movie-card.tsx
@@ -1,29 +1,32 @@
+import type { Movie } from '@/types'
 import Link from 'next/link'
 import Image from 'next/image'
-import { Star, Poster } from '@/images'
+import { Star } from '@/images'
 
-interface MovieCardProps {
-  image: string
-  text: string
-}
-
-export default function MovieCard({ text }: MovieCardProps) {
+export default function MovieCard({ id, poster_path, original_title, release_date, vote_average, overview }: Movie) {
   return (
     <Link
-      href='/'
+      href={`/${id}`}
       className='relative mx-auto h-96 w-3/4 overflow-hidden rounded-md border-2 border-yellow-500 transition-all hover:opacity-90 active:scale-95 sm:mx-0 sm:w-full'
+      title={original_title}
     >
-      <Image src={Poster} alt='poster' className='w-full contrast-125' />
+      <Image
+        src={`https://image.tmdb.org/t/p/w500${poster_path}`}
+        alt='poster'
+        className='w-full contrast-125'
+        width={500}
+        height={750}
+      />
       <div className='absolute top-0 size-full bg-gradient-to-t from-black to-transparent' />
       <div className='absolute top-0 flex h-full flex-col justify-end p-2 text-xs'>
-        <h3 className='text-base font-bold text-yellow-500'>Movie Name</h3>
+        <h3 className='text-base font-semibold text-yellow-500'>{original_title}</h3>
         <div className='flex w-full items-center justify-between font-medium text-yellow-500'>
-          <span>10-20-2020</span>
+          <span>{release_date}</span>
           <span>
-            8.0 <Image src={Star} alt='rating icon' className='inline w-[0.6rem] align-baseline' />
+            {vote_average} <Image src={Star} alt='rating icon' className='inline w-[0.6rem] align-baseline' />
           </span>
         </div>
-        <p className='line-clamp-2 italic tracking-wide'>{text}</p>
+        <p className='line-clamp-3 text-justify italic tracking-wide'>{overview}</p>
       </div>
     </Link>
   )
