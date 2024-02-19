38adaaa369cbef21f0251d1a396854326562ffd8
esteban-vm <estebanvm90@outlook.com>
2024-02-19 08:49:12 -0300

    update request function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,34 +2,34 @@ import type { Movie } from 'tmdb-ts'
 import { NextResponse } from 'next/server'
 import { TMDB } from 'tmdb-ts'
 
-const tmdb = new TMDB(process.env.ACCESS_TOKEN!)
+const { movies } = new TMDB(process.env.ACCESS_TOKEN!)
 type MovieListName = 'popular' | 'top_rated' | 'upcoming'
 
 export const handleRequest = (list: MovieListName) => {
   return async () => {
     try {
-      let movies: Movie[]
+      let results: Movie[]
 
       switch (list) {
         case 'popular': {
-          void ({ results: movies } = await tmdb.movies.popular())
+          void ({ results } = await movies.popular())
           break
         }
 
         case 'top_rated': {
-          void ({ results: movies } = await tmdb.movies.topRated())
+          void ({ results } = await movies.topRated())
           break
         }
 
         case 'upcoming': {
-          void ({ results: movies } = await tmdb.movies.upcoming())
+          void ({ results } = await movies.upcoming())
           break
         }
       }
 
-      return NextResponse.json(movies)
+      return NextResponse.json(results)
     } catch {
-      return []
+      return null
     }
   }
 }
