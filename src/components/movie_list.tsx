a4ba34ac1abff08f6e29c686aa4d44b7bbe0bb8f
esteban-vm <estebanvm90@outlook.com>
2024-02-20 08:52:07 -0300

    change names for some files

--- src/components/movie_list.tsx
+++ src/components/movie_list.tsx
@@ -1,10 +1,6 @@
 import type { ReactNode } from 'react'
 
-interface MovieListProps {
-  children: ReactNode
-}
-
-export default function MovieList({ children }: MovieListProps) {
+export default function MovieList({ children }: { children: ReactNode }) {
   return (
     <div className='grid h-full gap-1 py-2 sm:grid-cols-2 sm:py-3 md:grid-cols-3 md:gap-2 md:py-3 lg:grid-cols-4 lg:py-4 xl:grid-cols-5 xl:py-5'>
       {children}
