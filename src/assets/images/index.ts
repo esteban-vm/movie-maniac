f9e452e388769b057b85e86b9c7d1e974224dba4
esteban-vm <estebanvm90@outlook.com>
2024-02-22 08:53:54 -0300

    add new image for header component

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,4 +1,5 @@
 export { default as Fire } from './fire.png'
 export { default as GlowingStart } from './glowing_star.png'
 export { default as PartyingFace } from './partying_face.png'
+export { default as SmilingFace } from './smiling_face.png'
 export { default as Star } from './star.png'
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,6 @@
 import type { MovieListName } from '@/utils'
 import { ListTitle } from '@/common'
-import { Fire } from '@/images'
+import { SmilingFace } from '@/images'
 
 function RatingList() {
   return (
@@ -34,8 +34,8 @@ export default function Header(props: { title: MovieListName }) {
     <header className='flex items-center justify-between text-sm sm:text-sm md:text-base'>
       <h2 className='text-sm font-medium capitalize text-yellow-500 sm:text-base md:text-lg lg:text-xl xl:text-2xl'>
         <ListTitle
-          image={Fire}
-          alt='fire'
+          image={SmilingFace}
+          alt='smiling face'
           className='ml-1 inline size-4 select-none align-text-top sm:size-5 md:size-6 lg:size-6 xl:size-7'
           {...props}
         />
